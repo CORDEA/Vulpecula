a0415451c783a6d83fb39b82004c93778b2bfa77
Yoshihiro Tanaka <contact@cordea.jp>
2020-12-01 10:13:35 +0900

    receive messages

--- Consumer/Program.fs
+++ Consumer/Program.fs
@@ -1,8 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
+open FSharp.Control
+open DotPulsar
+
+let printMessage (message: Message) = message.Data.ToString() |> printfn "%s"
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let options = ConsumerOptions("", "")
+
+    let client =
+        PulsarClient.Builder().Build().CreateConsumer(options)
+
+    client.Messages()
+    |> AsyncSeq.ofAsyncEnum
+    |> AsyncSeq.map printMessage
+    |> AsyncSeq.toArraySynchronously
+    |> ignore
+
     0 // return an integer exit code
